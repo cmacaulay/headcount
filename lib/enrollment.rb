01ec9d73f2b963715558685e878ae828567645f6
Casey Macaulay <caseyannemacaulay@gmail.com>
2017-01-06 23:22:17 -0700

    adds enrollment repo. desperately needs testing.

--- lib/enrollment.rb
+++ lib/enrollment.rb
@@ -3,15 +3,13 @@ require 'pry'
 
 class Enrollment
   attr_reader :name,
-              :kindergarten_participation,
+              :kindergarten,
               :high_school_graduation
 
   def initialize(data)
     @name = data[:name].upcase
-    @kindergarten_participation =
-      data[:kindergarten_participation]
-    @high_school_graduation =
-      data[:high_school_graduation]
-  end
+    @kindergarten = data[:kindergarten]
+    @high_school_graduation = data[:high_school_graduation_rates]
+    end
 
 end
