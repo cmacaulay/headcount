5108cb63e2a7ffe5cf61e8a4294ff651c8468ff7
Casey Macaulay <caseyannemacaulay@gmail.com>
2017-01-07 23:47:51 -0700

    gets hacky but adds kindergarten_participation_rate_variation_trend. data realllly needs to be sanitized...

--- lib/headcount_analyst.rb
+++ lib/headcount_analyst.rb
@@ -6,15 +6,15 @@ class HeadcountAnalyst
     @district_repository = district_repository.districts
   end
 
-  def kindergarten_participation_rate_variation(first_district, second_district)
-    # first_district average / second_district average
-    # calculate_enrollment_average(first_district) / calculate_enrollment_average(second_district)
+  def kindergarten_participation_rate_variation(first_district, against_district)
+    # first_district average / against_district average
+    # calculate_enrollment_average(first_district) / calculate_enrollment_average(against_district)
     #use fetch(:against) to get 2nd district name
 
     first_district = district_repository.fetch(first_district)
-    second_district = district_repository.fetch(second_district.fetch(:against))
+    against_district = district_repository.fetch(against_district.fetch(:against))
 
-    rate = calculate_enrollment_average(first_district) / calculate_enrollment_average(second_district)
+    rate = calculate_enrollment_average(first_district) / calculate_enrollment_average(against_district)
     rate.round(3)
 
   end
@@ -30,14 +30,25 @@ class HeadcountAnalyst
     # sum of all the enrollment rates / # of yrs we have data for
   end
 
-  def kindergarten_participation_rate_variation_trend(fist_district, second_district)
+  def kindergarten_participation_rate_variation_trend(first_district, state_data)
     # comparing district enrollment to state average by year
     # district_one.merge(state_data)
     # do |year, district_data, state_data|
     # district_data/state_data
+    first_district = access_data( district_repository.fetch( first_district ) )
+    state_data = access_data( district_repository.fetch(state_data.fetch( :against ) ) )
 
-    
+    first_district.merge(state_data) do |year, district_rate, state_rate|
+      district_rate = district_rate.to_f
+      state_rate    = state_rate.to_f
+      
+      comparison = district_rate / state_rate
+      comparison.round(3)
+    end
+  end
 
+  def access_data(enrollment_object)
+    enrollment_object.enrollment.kindergarten
   end
 
 end
--- test/headcount_analyst_test.rb
+++ test/headcount_analyst_test.rb
@@ -9,17 +9,28 @@ class HeadcountAnalystTest < Minitest::Test
   def test_it_exists
     dr = DistrictRepository.new
     dr.load_data({:enrollment => {:kindergarten => "./test/fixtures/Kindergartners in full-day program.csv"}})
-    hca = HeadcountAnalyst.new(dr)
-    assert_instance_of HeadcountAnalyst, hca
+    ha = HeadcountAnalyst.new(dr)
+    assert_instance_of HeadcountAnalyst, ha
   end
 
   def test_kindergarten_participation_rate_variation_works
     dr = DistrictRepository.new
     dr.load_data({:enrollment => {:kindergarten => "./test/fixtures/Kindergartners in full-day program.csv"}})
-    hca = HeadcountAnalyst.new(dr)
-    assert_equal 1.353, hca.kindergarten_participation_rate_variation("ACADEMY 20", :against => "ADAMS COUNTY 14")
-    assert_equal 0.766, hca.kindergarten_participation_rate_variation("ACADEMY 20", :against => "COLORADO")
+    ha = HeadcountAnalyst.new(dr)
+    assert_equal 1.353, ha.kindergarten_participation_rate_variation("ACADEMY 20", :against => "ADAMS COUNTY 14")
+    assert_equal 0.766, ha.kindergarten_participation_rate_variation("ACADEMY 20", :against => "COLORADO")
   end
+
+  def test_kindergarten_participation_rate_variation_trend
+    dr = DistrictRepository.new
+    dr.load_data({:enrollment => {:kindergarten => "./test/fixtures/Kindergartners in full-day program.csv"}})
+    ha = HeadcountAnalyst.new(dr)
+    expected =
+    {2004 => 1.257, 2005 => 0.96, 2006 => 1.05, 2007 => 0.992, 2008 => 0.717, 2009 => 0.652, 2010 => 0.681, 2011 => 0.727, 2012 => 0.688, 2013 => 0.694, 2014 => 0.661 }
+
+    assert_equal expected, ha.kindergarten_participation_rate_variation_trend('ACADEMY 20', :against => 'COLORADO')
+  end
+
 
   # def test_enrollment_analysis_basics
   #   dr = DistrictRepository.new
