19c5989a677ade36916c517ac0db2608c6a56511
James Anderson <james.p.anderson16@gmail.com>
2017-01-12 07:55:56 -0700

    clean 1 sanitation issue

--- lib/result_entry.rb
+++ lib/result_entry.rb
@@ -4,11 +4,11 @@ class ResultEntry
                 :high_school_graduation_rate,
                 :median_household_income
 
-  def initialize(data)
-    @free_and_reduced_price_lunch_rate = data[:free_and_reduced_price_lunch_rate]
-    @children_in_poverty_rate          = data[:children_in_poverty_rate]
-    @high_school_graduation_rate       = data[:high_school_graduation_rate]
-    @median_household_income           = data[:median_household_income]
+  def initialize(i)
+    @free_and_reduced_price_lunch_rate = i[:free_and_reduced_price_lunch_rate]
+    @children_in_poverty_rate          = i[:children_in_poverty_rate]
+    @high_school_graduation_rate       = i[:high_school_graduation_rate]
+    @median_household_income           = i[:median_household_income]
     # @name
   end
 
